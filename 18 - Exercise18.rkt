541e815f534cc5dda059df8cf22d96dd46c69c2e
PeterCsik <62175713+PeterCsik@users.noreply.github.com>
2023-03-25 13:35:41 +0100

    Update 18 - Exercise18.rkt

--- 18 - Exercise18.rkt
+++ 18 - Exercise18.rkt
@@ -1,5 +1,3 @@
-(define (image-classify picture)(if (=(image-height picture)(image-width picture))
-                                    "square"
-                                    (if (>(image-height picture)(image-width picture))
-                                        "tall"
-                                        "wide")))
+(define (string-join string1 string2) (if(and(string? string1) (string? string2))
+                                         (string-append string1 "_" string2)
+                                         "Error: One of the or both of the inputs are not strings."))
