df57da38770c5899d061862388801b92eeffdf4f
PeterCsik <62175713+PeterCsik@users.noreply.github.com>
2023-03-27 13:51:49 +0200

    Create 028 - Exercise28.rkt

---
+++ 028 - Exercise28.rkt
@@ -0,0 +1,21 @@
+> (profit 1)
+511.2
+> (profit 2)
+937.2
+> (profit 3)
+1063.2
+> (profit 4)
+889.2
+> (profit 5)
+415.2
+
+> (profit 3.1)
+1059.3
+> (profit 3)
+1063.2
+> (profit 2.9) ;; the best ticket price
+1064.1
+> (profit 2.8)
+1062
+> (profit 2.7)
+1056.9
