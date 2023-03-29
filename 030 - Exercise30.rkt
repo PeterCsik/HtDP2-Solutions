47e0f382a6e9ec2ddb07feaddc29b7fb827e6f43
PeterCsik <62175713+PeterCsik@users.noreply.github.com>
2023-03-29 07:39:15 +0200

    Create 030 - Exercise30.rkt

---
+++ 030 - Exercise30.rkt
@@ -0,0 +1,20 @@
+(define fixed-cost 180)
+(define variable-cost 0.04)
+(define base-attendance 120)
+(define base-ticket-price 5.0)
+(define ticket-price-change 0.1)
+(define attendance-change 15)
+(define PRICE-SENSITIVITY (/ attendance-change ticket-price-change))
+
+(define (attendees ticket-price)
+  (- base-attendance (* (- ticket-price base-ticket-price) PRICE-SENSITIVITY)))
+
+(define (revenue ticket-price)
+  (* ticket-price (attendees ticket-price)))
+
+(define (cost ticket-price)
+  (+ fixed-cost (* variable-cost (attendees ticket-price))))
+
+(define (profit ticket-price)
+  (- (revenue ticket-price)
+     (cost ticket-price)))
