fc204a9ed3a7e2406b2032a2523542bf0486fb59
PeterCsik <62175713+PeterCsik@users.noreply.github.com>
2023-04-15 16:57:50 +0200

    Create 041 - Exercise41.rkt

---
+++ 041 - Exercise41.rkt
@@ -0,0 +1,69 @@
+(define WHEEL-RADIUS 15)
+
+
+(define WIDTH-OF-WORLD (* 40 WHEEL-RADIUS))
+
+(define HEIGHT-OF-WORLD (* 5 WHEEL-RADIUS))
+
+(define CROWN-OF-TREE  (circle (* 1.5 WHEEL-RADIUS) "solid" "green"))
+
+(define TRUNK-OF-TREE  (rectangle (* 0.25 WHEEL-RADIUS) (* 2.5 WHEEL-RADIUS) "solid" "brown"))
+
+(define TREE (underlay/align/offset "middle" "top"
+              CROWN-OF-TREE
+              0 (* 2 WHEEL-RADIUS)
+              TRUNK-OF-TREE))
+
+(define BACKGROUND (overlay/offset
+                    TREE
+                   (* 8 WHEEL-RADIUS) 0
+                   (empty-scene WIDTH-OF-WORLD HEIGHT-OF-WORLD)))
+
+(define WHEEL
+  (circle WHEEL-RADIUS "solid" "black"))
+
+(define SPACE
+  (rectangle (* WHEEL-RADIUS 3) 0 "outline" "white"))
+
+(define BOTH-WHEELS
+  (beside WHEEL SPACE WHEEL))
+
+(define CAR-ROOF
+  (rectangle (* 4 WHEEL-RADIUS) (* 1 WHEEL-RADIUS) "solid" "red"))
+
+(define CAR-PART
+  (rectangle (* 4 WHEEL-RADIUS) (* 2 WHEEL-RADIUS) "solid" "red"))
+
+(define CAR-PART-WITH-WHEEL
+  (underlay/xy
+   CAR-PART
+    WHEEL-RADIUS WHEEL-RADIUS
+   WHEEL))
+
+(define CAR
+  (above
+   CAR-ROOF
+   (beside
+    CAR-PART-WITH-WHEEL
+    CAR-PART-WITH-WHEEL)))
+
+(define Y-CAR (+(- (image-height BACKGROUND) (image-height CAR)) (/ (image-height CAR) 2)))
+
+(define (tock ws)
+  (+ ws 3))
+(check-expect (tock 20) 23)
+(check-expect (tock 100) 103)
+
+(define (render ws)
+  (place-image CAR ws Y-CAR BACKGROUND))
+(check-expect (render 50) (place-image CAR 50 Y-CAR BACKGROUND))
+(check-expect (render 90) (place-image CAR 90 Y-CAR BACKGROUND))
+
+(define (end? ws)
+  (> ws(+ (image-width BACKGROUND) (image-width CAR))))
+
+(define (main ws)
+  (big-bang ws
+    [to-draw render]
+    [on-tick tock]
+    [stop-when end?]))
