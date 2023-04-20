40dce41bd631379e4c78bcbfdf22a1d387ac8c7e
PeterCsik <62175713+PeterCsik@users.noreply.github.com>
2023-04-20 22:52:31 +0200

    Update 034 - Exercise34.rkt

--- 034 - Exercise34.rkt
+++ 034 - Exercise34.rkt
@@ -1,16 +1,13 @@
-; We use strings to represent non-empty strings
-; We use strings to represent single characters
+;; String -> String
+;; Extracts the first character from a non-empty string
 
-; String -> String
-; extracts the first character from a non-empty string
-; (define (f s) "c")
+(check-expect (string-first "abc") "a")
+(check-expect (string-first "hello")"h")
 
-; given: "Hello", expected: "H"
-; given: "bridge", expected: "b"
+;(define (string-first str) "a")        ;this is the stub
 
-; (define (string-first s) (... s ...))
+;(define (string-first str)             ;this is the template 
+;  (...str))
 
-(define (string-first s) (substring s 0 1))
-
-(string-first "Hello")
-(string-first "bridge")
+(define (string-first str)
+  (substring str 0 1))
