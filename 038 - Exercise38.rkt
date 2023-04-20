d5367f3aa3c8ef03c6e75ada0a0fbc81c5e8e05e
PeterCsik <62175713+PeterCsik@users.noreply.github.com>
2023-04-20 23:09:07 +0200

    Update 038 - Exercise38.rkt

--- 038 - Exercise38.rkt
+++ 038 - Exercise38.rkt
@@ -1,18 +1,13 @@
-; we use string to represent strings (textual information)
-; we use string to represent single characters
+;; String -> String
+;; Produces the given string with the last character removed.
 
-; String -> String
-; removes the last character from a string
-; (define (f s) "a")
+(check-expect (string-remove-last "abc") "ab")
+(check-expect (string-remove-last "hello") "hell")
 
-; given: "New York", expected: "New Yor"
-; given: "bus", expected "bu"
-; given: "x", expected ""
+;(define (string-remove-last str) "a")      ;this is the stub
 
-; (define (string-remove-last s) ( ... s ... )
+;(define (string-remove-last str)           ;this is the template
+;  (... str))
 
-(define (string-remove-last s) (substring s 0 (- (string-length s) 1)))
-
-(string-remove-last "New York")
-(string-remove-last "bus")
-(string-remove-last "a")
+(define (string-remove-last str)
+  (substring str 0 (- (string-length str) 1)))
