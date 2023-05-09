51ee559f40e14190f65a06c322ac9416416bc1e1
PeterCsik <62175713+PeterCsik@users.noreply.github.com>
2023-05-09 16:50:51 +0200

    Update 018 - Exercise18.rkt

--- 018 - Exercise18.rkt
+++ 018 - Exercise18.rkt
@@ -1,3 +1,2 @@
-(define (string-join string1 string2) (if(and(string? string1) (string? string2))
-                                         (string-append string1 "_" string2)
-                                         "Error: One of the or both of the inputs are not strings."))
+(define (string-join string1 string2)
+  (string-append string1 "_" string2))
