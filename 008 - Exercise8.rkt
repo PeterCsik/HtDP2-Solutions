7131d14a9d7ab0d70423765c7507f05665676ae4
PeterCsik <62175713+PeterCsik@users.noreply.github.com>
2023-05-09 14:30:10 +0200

    Update 008 - Exercise8.rkt

--- 008 - Exercise8.rkt
+++ 008 - Exercise8.rkt
@@ -1,2 +1,9 @@
-(define picture _ )
-(if (=(image-height picture)(image-width picture)) "square"(if (>(image-height picture)(image-width picture))"tall" "wide"))
+(define picture ... )
+
+(if (=(image-height picture)(image-width picture)) "square"
+(if (>(image-height picture)(image-width picture))"tall" "wide"))
+
+(cond                                                                    ;; an alternative solution using cond expressions
+    [ (=(image-height picture)(image-width picture)) "square"]
+    [ (>(image-height picture)(image-width picture)) "tall"]
+    [ else "wide"])
