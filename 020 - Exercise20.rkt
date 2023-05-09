46144fa6cc126fdea1d83331c12e1dcd8b41106e
PeterCsik <62175713+PeterCsik@users.noreply.github.com>
2023-05-09 18:30:11 +0200

    Update 020 - Exercise20.rkt

--- 020 - Exercise20.rkt
+++ 020 - Exercise20.rkt
@@ -1,6 +1,6 @@
 (define (string-delete str i)
   (cond
-    [(or (= i 0)(> i (string-length str)))(string-append "Number "(number->string i)" is out of the valid range [1, " (number->string(string-length str)) "].")]
+    [(or (< i 0)(>= i (string-length str)))(string-append "Number "(number->string i)" is out of the valid range [0, " (number->string(-(string-length str)1)) "].")]
     [(=(string-length str) 0) "Error: It is an empty string"]
-    [(and (> (string-length str) 0)(<= i (string-length str)))(string-append(substring str 0 (- i 1))""(substring str i (string-length str)))]
+    [(and (> (string-length str) 0)(<= i (string-length str)))(string-append(substring str 0 i)""(substring str (+ i 1) (string-length str)))]
     ))
