e13eb665448d23b9abb80a2d72ff93c688c3ca4f
PeterCsik <62175713+PeterCsik@users.noreply.github.com>
2023-05-09 14:57:37 +0200

    Update 009 - Exercise9.rkt

--- 009 - Exercise9.rkt
+++ 009 - Exercise9.rkt
@@ -4,3 +4,16 @@
 (if (image? in) (* (image-width in) (image-height in))
 (if (boolean? in) (if in 10 20)
 (if (and (number? in) (> in 0)) (- in 1) in))))
+
+(cond                                                       ;; an  alternative solution using cond expressions
+  [(string? in) (string-length in)]
+  [(image? in) (* (image-width in)(image-height in))]
+  [(boolean? in)
+   (cond
+     [(and in #true) 10]
+     [else 20])]
+  [(number? in)
+   (cond
+     [(> in 0) (- in 1)]
+     [(<= in 0) in])]
+  )
