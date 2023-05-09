471e8ad26dc07e883cd66b0224925f811d0fac80
PeterCsik <62175713+PeterCsik@users.noreply.github.com>
2023-05-09 15:47:59 +0200

    Update 013 - Exercise13.rkt

--- 013 - Exercise13.rkt
+++ 013 - Exercise13.rkt
@@ -1,7 +1,7 @@
 (define (string-first x)
   (if (string? x) (if(> (string-length x) 0) 
-                                      (string-ith x 0) 
-                                      "Error: It is an empty string.") 
+                        (string-ith x 0) 
+                        "Error: It is an empty string.") 
                               "Error: It is not a string."))
 
 (define (string-first x)
