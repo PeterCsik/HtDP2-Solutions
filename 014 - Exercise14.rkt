13eefc1e1d88078cbd24be7b4310f3b2d6aad114
PeterCsik <62175713+PeterCsik@users.noreply.github.com>
2023-05-09 16:33:13 +0200

    Update 014 - Exercise14.rkt

--- 014 - Exercise14.rkt
+++ 014 - Exercise14.rkt
@@ -3,3 +3,13 @@
                                       (string-ith x (- (string-length x) 1)) 
                                       "Error: It is an empty string.") 
                               "Error: It is not a string."))
+
+(define (string-last x)                         ;; an alternative solution using cond expressions
+  (cond
+    [(string? x)
+         (cond
+           [(> (string-length x) 0) (string-ith x (- (string-length x)1))]
+           [(= (string-length x) 0) "Error: It is an empty string."])]
+       [ else "Error: It is not a string."]
+    )
+  )
