daf445643b27e636e76b9b817f16a30a1a1370e8
PeterCsik <62175713+PeterCsik@users.noreply.github.com>
2023-05-10 08:24:09 +0200

    Create 022- Exercise22.rkt

---
+++ 0272- Exercise22.rkt
@@ -0,0 +1,9 @@
+(define (distance-to-origin x y)
+  (sqrt (+ (sqr x) (sqr y))))
+  
+(distance-to-origin 3 4)         ; returns 5
+; (sqrt (+ (sqr 3) (sqr 4)))
+; (sqrt (+ 9 (sqr 4)))
+; (sqrt (+ 9 16))
+; (sqrt 25)
+; 5
