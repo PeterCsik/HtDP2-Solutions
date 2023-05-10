70ab7948ba0e3df8796ca573173eb85cf26d681b
PeterCsik <62175713+PeterCsik@users.noreply.github.com>
2023-05-10 11:17:18 +0200

    Update 027 - Exercise27.rkt

--- 027 - Exercise27.rkt
+++ 027 - Exercise27.rkt
@@ -1,18 +1,18 @@
-(define fixed-cost 180)
-(define variable-cost 0.04)
-(define base-attendance 120)
-(define base-ticket-price 5.0)
-(define ticket-price-change 0.1)
-(define attendance-change 15)
+(define BASE-ATTENDANCE 120)
+(define BASE-TICKET-PRICE 5.0)
+(define ATTENDANCE-CHANGE 15)
+(define TICKET-PRICE-CHANGE 0.1)
+(define FIXED-COST 180)
+(define VARIABLE-COST 0.04)
 
 (define (attendees ticket-price)
-  (- base-attendance (* (- ticket-price base-ticket-price) (/ attendance-change ticket-price-change))))
+  (- BASE-ATTENDANCE (* (- ticket-price BASE-TICKET-PRICE) (/ ATTENDANCE-CHANGE TICKET-PRICE-CHANGE))))
 
 (define (revenue ticket-price)
   (* ticket-price (attendees ticket-price)))
 
 (define (cost ticket-price)
-  (+ fixed-cost (* variable-cost (attendees ticket-price))))
+  (+ FIXED-COST (* VARIABLE-COST (attendees ticket-price))))
 
 (define (profit ticket-price)
   (- (revenue ticket-price)
