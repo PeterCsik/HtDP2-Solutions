90d9041451266a63609ca05b6760c9d58d0c23fc
PeterCsik <62175713+PeterCsik@users.noreply.github.com>
2023-05-10 11:31:34 +0200

    Update 029 - Exercise29.rkt

--- 029 - Exercise29.rkt
+++ 029 - Exercise29.rkt
@@ -1,28 +1,24 @@
-(define fixed-cost 0)
-(define variable-cost 1.5)
-(define base-attendance 120)
-(define base-ticket-price 5.0)
-(define ticket-price-change 0.1)
-(define attendance-change 15)
+(define BASE-ATTENDANCE 120)
+(define BASE-TICKET-PRICE 5.0)
+(define ATTENDANCE-CHANGE 15)
+(define TICKET-PRICE-CHANGE 0.1)
+(define FIXED-COST 0.00)
+(define VARIABLE-COST 1.50)
 
 (define (attendees ticket-price)
-  (- base-attendance (* (- ticket-price base-ticket-price) (/ attendance-change ticket-price-change))))
+  (- BASE-ATTENDANCE (* (- ticket-price BASE-TICKET-PRICE) (/ ATTENDANCE-CHANGE TICKET-PRICE-CHANGE))))
 
 (define (revenue ticket-price)
   (* ticket-price (attendees ticket-price)))
 
 (define (cost ticket-price)
-  (+ fixed-cost (* variable-cost (attendees ticket-price))))
+  (+ FIXED-COST (* VARIABLE-COST (attendees ticket-price))))
 
 (define (profit ticket-price)
   (- (revenue ticket-price)
      (cost ticket-price)))
 
-;; Application:
 
-> (profit 3) ;; before 1063.2
-630
-> (profit 4) ;; before 889.2
-675
-> (profit 5) ;; before 415.2
-420
+(profit 3)        ; returns 630, before 1063.2
+(profit 4)        ; returns 675, before 889.2
+(profit 5)        ; returns 420, before 415.2
