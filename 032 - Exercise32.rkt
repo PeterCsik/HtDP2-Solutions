221047b896cebe81a248ed7b5e04ba58c424b6f2
PeterCsik <62175713+PeterCsik@users.noreply.github.com>
2023-05-10 16:08:04 +0200

    Create 032 - Exercise32.rkt

---
+++ 032 - Exercise32.rkt
@@ -0,0 +1,10 @@
+; air temperature change
+; recognization of familiar faces
+; automatic pay for a public transport ticket once one has entered the vehicle
+; recognization friends nearby based on their smart watches
+; location tracking
+; scanning of items during shopping and then paying for them at self-checkout
+; incoming message (alerting)
+; visual note taking
+; measuring key health indicators
+; reporting dirt and dangerous items in the streets
