e69bf027cc7358ac398cab03195bb6cd25d5d6d9
PeterCsik <62175713+PeterCsik@users.noreply.github.com>
2023-05-11 16:33:14 +0200

    Update 034 - Exercise34.rkt

--- 034 - Exercise34.rkt
+++ 034 - Exercise34.rkt
@@ -1,13 +1,20 @@
-;; String -> String
-;; Extracts the first character from a non-empty string
+; A non-empty string is represented by String (data type).
+; Interpretation: A non-empty string is a sequence of characters with at least one character.
 
-(check-expect (string-first "abc") "a")
-(check-expect (string-first "hello")"h")
+; Non-empty String -> String
+; Extracts the first character from a non-empty string.
+; (define (string-first str) "a")
 
-;(define (string-first str) "a")        ;this is the stub
+; given: "abc", expected: "a"
+; given: "hello world", expected: "h"
+; given: "b", expected: "b"
 
-;(define (string-first str)             ;this is the template 
-;  (...str))
+;(define (string-first str)
+;  (... str ...))
 
 (define (string-first str)
   (substring str 0 1))
+
+(check-expect (string-first "abc") "a")
+(check-expect (string-first "hellow world") "h")
+(check-expect (string-first "b") "b")
