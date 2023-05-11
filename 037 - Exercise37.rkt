bf5dc9562c2734990dde4e805d3a078dc0371072
PeterCsik <62175713+PeterCsik@users.noreply.github.com>
2023-05-11 17:12:15 +0200

    Update 037 - Exercise37.rkt

--- 037 - Exercise37.rkt
+++ 037 - Exercise37.rkt
@@ -1,13 +1,20 @@
-;; String -> String
-;; Produces the give string with the first character removed.
+; A non-empty string is represented by String (data type).
+; Interpretation: a non-empty string is a sequence of characters with at least one character.
 
-(check-expect (string-rest "abc") "bc")
-(check-expect (string-rest "hello") "ello")
+; (Non-empty) String -> String
+; Produces a string with the first character removed.
+; (define (string-first str) "a")
 
-;(define (string-rest str) "a")        ;this is the stub
+; given: "abc", expected: "bc"
+; given: "hello world", expected: "ello world"
+; given: "b", expected: ""
 
-;(define (string-rest str)             ;this the template
-;  (... str))
+; (define (string-first str)
+;   (... str ...))
 
-(define (string-rest str)
-  (substring str 1))
+(define (string-first str)
+  (substring str 1 (string-length str)))
+
+(check-expect (string-first "abc") "bc")
+(check-expect (string-first "hello world") "ello world")
+(check-expect (string-first "b") "")
