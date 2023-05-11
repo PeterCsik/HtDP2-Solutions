3906a1d81c0b97e8a7a524360055fe72bbd3ac14
PeterCsik <62175713+PeterCsik@users.noreply.github.com>
2023-05-11 16:43:46 +0200

    Update 035 - Exercise35.rkt

--- 035 - Exercise35.rkt
+++ 035 - Exercise35.rkt
@@ -1,13 +1,20 @@
-;; String -> String
-;; Extracts the last character from a non-empty string.
+; A non-empty string is represented by String (data type).
+; Interpretation: A non-empty string is a sequence of characters with at least one character.
 
-(check-expect (string-last "abc") "c")
-(check-expect (string-last "hello") "o")
+; (Non-empty) String -> String
+; Extracts the last character from a non-empty string.
+; (define (string-last str) "a")
 
-;(define (string-last str) "a")       ;this is the stub
+; given: "abc", expected: "c"
+; given: "hello world", expected: "d"
+; given: "b", expected: "b"
 
-;(define (string-last str)            ;this is the template
-;  (... str))
+; (define (string-last str)
+;   (... str ...))
 
 (define (string-last str)
   (string-ith str (-(string-length str)1)))
+
+(check-expect (string-last "abc") "c")
+(check-expect (string-last "hello world") "d")
+(check-expect (string-last "b") "b")
