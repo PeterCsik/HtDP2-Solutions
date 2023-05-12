e2199ac8c35fa708596a152639fcbcfdfa88e3c1
PeterCsik <62175713+PeterCsik@users.noreply.github.com>
2023-05-12 23:58:17 +0200

    Update 040 - Exercise40.rkt

--- 040 - Exercise40.rkt
+++ 040 - Exercise40.rkt
@@ -1,6 +1,6 @@
 (define (tock ws)
   (+ ws 3))
 
-(check-expect (tock 20) 23)           ; returns the expected value: 23
-(check-expect (tock 100) 103)         ; returns the expected value: 103
-(check-expect (tock 200) 210)         ; test failed, it does not return the expected value: 203
+(check-expect (tock 20) 23)           ; actual value 23 agrees with 23, the expected value
+(check-expect (tock 100) 103)         ; actual value 103 agrees with 103, the expected value
+(check-expect (tock 200) 210)         ; a check failure: Actual value 203 differs from 210, the expected value
