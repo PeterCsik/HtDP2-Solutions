b976e8b39d66c85c88be1c7263ab2402620e3881
PeterCsik <62175713+PeterCsik@users.noreply.github.com>
2023-05-12 23:58:49 +0200

    Update 040 - Exercise40.rkt

--- 040 - Exercise40.rkt
+++ 040 - Exercise40.rkt
@@ -3,4 +3,4 @@
 
 (check-expect (tock 20) 23)           ; actual value 23 agrees with 23, the expected value
 (check-expect (tock 100) 103)         ; actual value 103 agrees with 103, the expected value
-(check-expect (tock 200) 210)         ; a check failure: Actual value 203 differs from 210, the expected value
+(check-expect (tock 200) 210)         ; a check failure: actual value 203 differs from 210, the expected value
