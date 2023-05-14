b782f08ef78afe9e0397c1544441af8644a6df0d
PeterCsik <62175713+PeterCsik@users.noreply.github.com>
2023-05-14 19:27:33 +0200

    Update 046 - Exercise46.rkt

--- 046 - Exercise46.rkt
+++ 046 - Exercise46.rkt
@@ -1,63 +1,70 @@
-(define CAT1 ...image1)
-(define CAT2 ...image2)
-(define CAT1-HEIGHT (image-height CAT1))
-(define CAT1-WIDTH (image-width CAT1))
+; ---------- Constant definitions ----------
+(define CAT1 .)
+(define CAT2 .)
 
-(define BACKGROUND (empty-scene (* CAT1-WIDTH 10) (+ CAT1-HEIGHT 10) ))
-(define BACKGROUND-HEIGHT (image-height BACKGROUND))
-(define BACKGROUND-WIDTH (image-width BACKGROUND))
+(define BACKGROUND
+  (empty-scene (*(image-width CAT1) 10) (*(image-height CAT1) 1.1)))
 
-;; WorldState is a Number
-;; interpretation: the number of pixels between the left border of the background and the cat
+(define X-CAT1
+  (round(/ (image-width CAT1)2)))
 
-;; Number -> Image
-;; places the image of the cat x pixels from the left margin of the BACKGROUND image
-;; if the x-coordinate is an odd number, use cat1 image, otherwise use cat2 image
-; (define (render ws) img)      ; this is the stub
+(define Y-CAT1
+  (/ (image-height CAT1)1.8))
 
-(check-expect (render 10)
-              (cond
-                [(odd? 10)(place-image CAT1 10 (/ BACKGROUND-HEIGHT 2) BACKGROUND)]
-                [else (place-image CAT2 10 (/ BACKGROUND-HEIGHT 2) BACKGROUND)]))
+; ---------- Function definitions ----------
 
-(check-expect (render 11)
-              (cond
-                [(odd? 11)(place-image CAT1 11 (/ BACKGROUND-HEIGHT 2) BACKGROUND)]
-                [else (place-image CAT2 11 (/ BACKGROUND-HEIGHT 2) BACKGROUND)]))
+; A WorldState is a Number. (cw)
+; Interpretation: the number of pixels between the left border of the scene and the left-most edge of the cat.
 
-;(define (render ws)           ; this is the template
-;    (BACKGROUND))
-
-(define (render ws)
-  (cond
-  [(odd? ws)(place-image CAT1 (modulo ws BACKGROUND-WIDTH) (/ BACKGROUND-HEIGHT 2) BACKGROUND)]
-  [else(place-image CAT2 (modulo ws BACKGROUND-WIDTH) (/ BACKGROUND-HEIGHT 2) BACKGROUND)]))
+; WorldState -> Image
+; Places the cat into the BACKGROUND according to the given world state.
+; Whenever the cat disappears on the right, it reappears on the left.
+; (define (render cw)
+;   (... modulo cw ...))
 
 
-;; WorldState -> WordlState
-;; moves the cat by 3 pixels for every clock tick
-; (define (tock ws) 0)     ; this is the stub
+; given: 11, expected CAT2 and 49
+; given: 1, expected CAT2 and 39
+; given: 801, expected CAT 2 and 89
 
-(check-expect (tock 1) 4)
+; given: 10, expected CAT2 and 48
+; given: 0, expected CAT2 and 38
+; given: 800, expected CAT 2 and 88
+
+(define (render cw)
+  (place-image
+   (cond [(odd? cw) CAT1]
+         [(even? cw) CAT2])
+         (modulo(+ cw X-CAT1)(image-width BACKGROUND)) Y-CAT1 BACKGROUND))
+
+(check-expect (render 11) (place-image CAT1 (modulo (+ 11 X-CAT1)(image-width BACKGROUND)) Y-CAT1 BACKGROUND))
+(check-expect (render 1) (place-image CAT1 (modulo (+ 1 X-CAT1)(image-width BACKGROUND)) Y-CAT1 BACKGROUND))
+(check-expect (render 801) (place-image CAT1 (modulo (+ 801 X-CAT1)(image-width BACKGROUND)) Y-CAT1 BACKGROUND))
+
+(check-expect (render 10) (place-image CAT2 (modulo (+ 10 X-CAT1)(image-width BACKGROUND)) Y-CAT1 BACKGROUND))
+(check-expect (render 0) (place-image CAT2 (modulo (+ 0 X-CAT1)(image-width BACKGROUND)) Y-CAT1 BACKGROUND))
+(check-expect (render 800) (place-image CAT2 (modulo (+ 800 X-CAT1)(image-width BACKGROUND)) Y-CAT1 BACKGROUND))
+
+; WorldState -> WorldState
+; Moves the cat by 3 pixels for every clock tick.
+; (define (tock cw) 0)
+
+; given: 10, expected 13
+; given: 0, expected 3
+
+; (define (tock cw)
+;   (... cw ...))
+
+(define (tock cw)
+  (+ cw 3))
+
+(check-expect (tock 10) 13)
 (check-expect (tock 0) 3)
 
-; (define (tock ws)       ; this is the template
-;    (... ws))
+; ---------- Application ----------
 
-(define (tock ws)
-  (+ ws 3))
-
-
-;; WorldState -> Image
-;; continuously moves the cat from left to right,
-;; whenever the cat disappears on the right, it reappears on the left
-; (define (cat-prog ws) img)    ; this the stub
-
-; (define (cat-prog ws)         ; this is the template
-;   (BACKGROUND))
-
-(define (cat-prog ws)
-  (big-bang ws
-      [to-draw render]
-      [on-tick tock]
+(define (cat-prog cw)
+  (big-bang cw
+    [to-draw render]
+    [on-tick tock]
   ))
