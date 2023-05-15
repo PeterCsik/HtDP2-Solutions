41d51a00f419f2a89cbc2b776c38d3a6c4cc1f8f
PeterCsik <62175713+PeterCsik@users.noreply.github.com>
2023-05-15 20:49:53 +0200

    Update 047 - Exercise47.rkt

--- 047 - Exercise47.rkt
+++ 047 - Exercise47.rkt
@@ -1,113 +1,112 @@
-(define BACKGROUND-HEIGHT 25)
+; ---------- Constant definitions ----------
+
 (define BACKGROUND-WIDTH 100)
+(define BACKGROUND-HEIGHT 25)
 (define BACKGROUND (rectangle BACKGROUND-WIDTH BACKGROUND-HEIGHT "outline" "black"))
 
+; ---------- Function definitions ----------
 
-;; WorldState is a number. (ws)
-;; interpretation represents the current level of of happiness
+; WorldState is a Number. (cw)
+; Interpretation: the number represents the current level of of happiness.
 
+; WorldState -> Image
+; Produces a red bar with a width according to the WorldState.
+; (define (red-bar cw) img)
 
-;; WorldState -> Image
-;; produces a red bar based on the WorldState
-; (define (red-bar ws) img)    ; this is the stub
+; given: 10, expected: (rectangle 10 BACKGROUND-HEIGHT "solid" "red"))
+; given: 80, expected: (rectangle 150 BACKGROUND-HEIGHT "solid" "red"))
 
-(check-expect (red-bar 10) (rectangle 10 25 "solid" "red"))
-(check-expect (red-bar 150) (rectangle 150 25 "solid" "red"))
-
-; (define (red-bar ws)        ; this is the template
-;    (...ws))
+; (define (red-bar cw)       
+;    (... cw ...))
 
 (define (red-bar ws) (rectangle ws BACKGROUND-HEIGHT "solid" "red"))
 
+(check-expect (red-bar 10) (rectangle 10 BACKGROUND-HEIGHT "solid" "red"))
+(check-expect (red-bar 80) (rectangle 80 BACKGROUND-HEIGHT "solid" "red"))
 
-;; WorldState -> Image
-;; produces a scene with red bar based on the WorldState
-; (define (scene ws) img)    ; this is the stub
+; WorldState -> Image
+; Produces a scene with a red bar in it based on the WorldState.
+; (define (render cw) img)
 
-(check-expect (scene 10) (overlay/align "left" "middle"
+; given: 10, expected: (overlay ... BACKGROUND (red-bar 10))
+; given: 80, expected:  (overlay ... BACKGROUND (red-bar 80))
+
+; (define (render cw)
+;    (... cw ...))
+
+(define (render cw) (overlay/align "left" "middle"
+               BACKGROUND
+               (red-bar cw)))
+
+(check-expect (render 10) (overlay/align "left" "middle"
                             BACKGROUND
                             (red-bar 10)))
                
-(check-expect (scene 100) (overlay/align "left" "middle"
+(check-expect (render 100) (overlay/align "left" "middle"
                             BACKGROUND
                             (red-bar 100)))
 
-; (define (scene ws)        ; this is the template
-;    (...img))
+; WorldState -> WordlState
+; Reduces the current WorldState by 0.1.
+; (define (clock-tick cw) #i0)
 
-(define (scene ws) (overlay/align "left" "middle"
-               BACKGROUND
-               (red-bar ws)))
+; given: 1, expected: 0.9
+; given: 5, expected: 4.9
 
+; (define (clock-tick cw)
+;    (... cw ...))
 
-;; Worldstate -> Image
-;; changes width of the red bar in the scene based on the WorldState 
-; (define (render ws) img)     ; this is the stub
-
-(check-expect (render 10)(scene 10))
-(check-expect (render 100) (scene 100))
-
-; (define (render ws)         ; this is the template
-;   (...scene))
-
-(define (render ws)
-  (scene ws))
-
-
-;; WorldState -> WordlState
-;; reduces the current WorldState by 0.1
-; (define (clock-tick ws) #i0)     ; this is the stub
+(define (clock-tick cw)
+  (- cw 0.1))
 
 (check-expect (clock-tick 1) (- 1 0.1))
 (check-expect (clock-tick 5) (- 5 0.1))
 
-; (define (clock-tick ws)       ; this is the template
-;    (... ws))
+; WorldState KeyEvent -> WordlState
+; Changes the WorldState using up and down arrow key events.
+; If the up arrow key is used, it increases the WorldState by 1/3.
+; If the down arrow key is used, it decreases the WorldState by 1/5.
+; (define (handle-key ws ke) #i0)
 
-(define (clock-tick ws)
-  (- ws 0.1))
+; given: 10 and up-arrow, expected: 10 + 1/3
+; given: 10 and down-arrow, expected: 10 - 1/5
 
+; (define (handle-key cw ke)
+;   (... cw ... key ...))
 
-;; WorldState KeyEvent -> WordlState
-;; changes the WorldState using up and down arrow key events
-;; if the up arrow key is used, it increases the WorldState by 1/3
-;; if the down arrow key is used, it decreases the WorldState by 1/5
-; (define (handle-key ws ke) #i0)     ; this is the stub
+(define (handle-key cw ke)
+  (cond
+    ((key=? ke "up") (+ cw 1/3))
+    ((key=? ke "down") (- cw 1/5))
+    (else cw)))
 
 (check-expect (handle-key 10 "up") (+ 10 1/3))
 (check-expect (handle-key 10 "down") (- 10 1/5))
 
-; (define (handle-key ws ke)          ; this is the template
-;           (...ws))
+; WorldState -> Boolean
+; Stops rendering when Worldstate is below 0, or above 100.
+; (define (end? cw) #false)
 
-(define (handle-key ws ke)
-  (cond
-    ((key=? ke "up") (+ ws 1/3))
-    ((key=? ke "down") (- ws 1/5))
-    (else ws)))
+; given: -1, expected: #true
+; given: 0, expected: #false
+; given: 101, expected: #true
+; given: 15, expected: #false
+   
+; (define (end? cw)
+;   (... cw ...))
 
+(define (end? cw)
+  (or (> cw BACKGROUND-WIDTH)(< cw 0)))
 
-;; end?
-;; WorldState -> Boolean
-;; stops renedering when Worldstate is below 0, or above 100
-;; (define (end? ws) #false)    ; this is the stub
-
+(check-expect (end? -1) #true)
 (check-expect (end? 0) #false)
 (check-expect (end? 101) #true)
 (check-expect (end? 15) #false)
-   
-; (define (end? ws)             ; this is the templates
-;     (... ws))
 
-(define (end? ws)
-  (or (> ws BACKGROUND-WIDTH)(< ws 0)))
+; ---------- Application ----------
 
-
-;; WorldState -> WordlState
-;; consumes the current level of happiness
-
-(define (gauge-prog ws)
-  (big-bang ws
+(define (gauge-prog cw)
+  (big-bang 100
       [to-draw render]
       [on-tick clock-tick]
       [on-key handle-key]
