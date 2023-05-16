2ea0e2f1ef07377fb3df72aab51e657cd584eab3
PeterCsik <62175713+PeterCsik@users.noreply.github.com>
2023-05-17 00:09:01 +0200

    Update 050 - Exercise50.rkt

--- 050 - Exercise50.rkt
+++ 050 - Exercise50.rkt
@@ -1,12 +1,34 @@
-; TrafficLight -> TrafficLight
-; yields the next state given current state s
+; ---------- Function definitions ----------
 
-(check-expect (traffic-light-next "red") "green")
-(check-expect (traffic-light-next "green") "yellow")
-(check-expect (traffic-light-next "yellow") "red")
+; A TrafficLight is one of the following Strings:
+; – "red"
+; – "green"
+; – "yellow"
+; Interpretation: the three strings represent the three possible states that a traffic light may assume.
+
+; TrafficLight -> TrafficLight
+; Yields the next state given current state s.
+; (define (traffic-light-next str) "a")
+
+; given: red, expected: green
+; given: green, expected: yellow
+; given: yellow, expected: red
+
+; (define (traffice-light-next s)
+;    ( ... s ...))
 
 (define (traffic-light-next s)
   (cond
     [(string=? "red" s) "green"]
     [(string=? "green" s) "yellow"]
     [(string=? "yellow" s) "red"]))
+
+(check-expect (traffic-light-next "red") "green")
+(check-expect (traffic-light-next "green") "yellow")
+(check-expect (traffic-light-next "yellow") "red")
+
+; ---------- Application ----------
+
+(traffic-light-next "red")
+(traffic-light-next "green")
+(traffic-light-next "yellow")
