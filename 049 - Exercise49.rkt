21bbc75ff271a2f5f142c4683ff68b4a005c4c50
PeterCsik <62175713+PeterCsik@users.noreply.github.com>
2023-05-16 23:55:30 +0200

    Update 049 - Exercise49.rkt

--- 049 - Exercise49.rkt
+++ 049 - Exercise49.rkt
@@ -1,13 +1,25 @@
+(- 200 (cond [(> 100 200) 0] [else y]))
+
+; (- 200 (cond [(> 100 200) 0] [else 100]))               ; (- 200 (cond (#false 0) (else 100)))
+                                                          ; (- 200 (cond (else 100)))
+                                                          ; (- 200 100)
+                                                          ; 100
+
+; (- 200 (cond [(> 210 200) 0] [else 210]))               ; (- 200 (cond (#true 0) (else 210)))
+                                                          ; (- 200 0)
+                                                          ; 200
+                                                          
+
 (define WIDTH  100)
 (define HEIGHT  60)
 (define MTSCN  (empty-scene WIDTH HEIGHT)) ; short for empty scene 
-(define ROCKET .)
+(define ROCKET ...)                                                 ; replace "..." with a picture of a racket
 (define ROCKET-CENTER-TO-TOP
   (- HEIGHT (/ (image-height ROCKET) 2)))
  
 (define (create-rocket-scene.v5 h)
   (place-image ROCKET 50
-  (cond
-    [(<= h ROCKET-CENTER-TO-TOP)h]
-    [(> h ROCKET-CENTER-TO-TOP)ROCKET-CENTER-TO-TOP]) 
-    MTSCN))
+  (cond 
+    [(<= h ROCKET-CENTER-TO-TOP) h]
+    [(> h ROCKET-CENTER-TO-TOP) ROCKET-CENTER-TO-TOP])
+  MTSCN))
