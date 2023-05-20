d20f892f4ff7b87533f5576fcaab38e25a4c0d1f
PeterCsik <62175713+PeterCsik@users.noreply.github.com>
2023-05-21 00:24:13 +0200

    Update 042 - Exercise42.rkt

--- 042 - Exercise42.rkt
+++ 042 - Exercise42.rkt
@@ -50,8 +50,7 @@
 
 ; WorldState -> Image
 ; Places the car into the SCENE according to the given world state.
-; (define (render cw)
-;    SCENE))
+; (define (render cw) img)
 
 ; given: 0, expected (place-image CAR 0 Y-CAR SCENE)
 ; given: 50, expected (place-image CAR 50 Y-CAR SCENE)
@@ -98,7 +97,7 @@
 (check-expect (end? 40) #false)
 (check-expect (end? 400) #true)
 
-; ---------- Application ----------
+; ---------- Application Function ----------
 
 (define (main cw)
   (big-bang cw
