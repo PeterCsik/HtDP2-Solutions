376dc8af50fd2db0e95b3dce94b44bcc3365dc14
PeterCsik <62175713+PeterCsik@users.noreply.github.com>
2023-05-20 10:10:49 +0200

    Update 054 - Exercise54.rkt

--- 054 - Exercise54.rkt
+++ 054 - Exercise54.rkt
@@ -7,6 +7,6 @@
  
  (define (show x)
   (cond
-    [(zero? x) ...]               ; To represent "resting" with a Number is to use 0 for WorldState 
+    [(zero? x) ...]               ; To represent "resting" with a Number (instead of a String) is to use 0 for WorldState 
     [(<= -3 x -1) ...]
     [(>= x 0) ...]))
