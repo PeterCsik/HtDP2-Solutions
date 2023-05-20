338ebeca15460dd132c21dd28d816d7ee9a6d8f1
PeterCsik <62175713+PeterCsik@users.noreply.github.com>
2023-05-21 00:13:15 +0200

    Update 008 - Exercise8.rkt

--- 008 - Exercise8.rkt
+++ 008 - Exercise8.rkt
@@ -3,7 +3,7 @@
 (if (=(image-height picture)(image-width picture)) "square"
 (if (>(image-height picture)(image-width picture))"tall" "wide"))
 
-(cond                                                                    ;; an alternative solution using cond expressions
+(cond                                                                    ; an alternative solution using cond expressions
     [ (=(image-height picture)(image-width picture)) "square"]
     [ (>(image-height picture)(image-width picture)) "tall"]
     [ else "wide"])
