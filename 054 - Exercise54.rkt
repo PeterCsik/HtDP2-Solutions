684022ada1a7df7a8ab08e39d88ecfca7bf72d08
PeterCsik <62175713+PeterCsik@users.noreply.github.com>
2023-05-20 10:09:30 +0200

    Create 054 - Exercise54.rkt

---
+++ 054 - Exercise54.rkt
@@ -0,0 +1,12 @@
+(define (show x)
+  (cond
+    [(string? x) ...]             ; It would be incorrect to use (string? x) condition, because big-bang function accepts only Number for a WorldState (x) 
+    [(<= -3 x -1) ...]
+    [(>= x 0) ...]))
+ 
+ 
+ (define (show x)
+  (cond
+    [(zero? x) ...]               ; To represent "resting" with a Number is to use 0 for WorldState 
+    [(<= -3 x -1) ...]
+    [(>= x 0) ...]))
