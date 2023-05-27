b015c0df69d3540f9e5dac99831d9dec7399a922
PeterCsik <62175713+PeterCsik@users.noreply.github.com>
2023-05-27 15:00:39 +0200

    Update 063 - Exercise63.rkt

--- 063 - Exercise63.rkt
+++ 063 - Exercise63.rkt
@@ -3,8 +3,24 @@
    (+ (sqr (posn-x ap))
       (sqr (posn-y ap)))))
  
-(distance-to-0 (make-posn 3 4))             ; result: 5
  
+(distance-to-0 (make-posn 3 4))             ; result: 5
+(sqrt
+  (+  (sqr (posn-x (make-posn 3 4))
+      (sqr (posn-y (make-posn 3 4)))))
+
+(sqrt
+  (+  (sqr 3)
+      (sqr (posn-y (make-posn 3 4)))))
+      
+(sqrt
+  (+ 9
+      (sqr 4)))
+      
+(sqrt (+ 9 16))
+(sqrt 25)
+5
+
 (distance-to-0 (make-posn 6 (+ 2 4)))       ; result: #i8.48528137423857
  
 (+ (distance-to-0 (make-posn 12 5)) 10)     ; #i8.48528137423857
