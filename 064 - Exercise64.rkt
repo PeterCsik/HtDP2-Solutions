932eff45b156b47920c1fecbdf19a47c0fb73634
PeterCsik <62175713+PeterCsik@users.noreply.github.com>
2023-05-27 16:28:52 +0200

    Create 064 - Exercise64.rkt

---
+++ 064 - Exercise64.rkt
@@ -0,0 +1,26 @@
+; posn is two Numbers
+; intepretation represents a position in a two-dimensional plane
+; the first number is called x-coordinate
+; the second number is called y-coordinate
+
+; Distance is a Number
+; interpretation represents a path that follows the rectangular grid
+; of streets found in Manhattan
+
+; posn -> Distance
+; measures the distance of the given posn to the origin
+; (define (manhattan-distance p) 0)
+
+(check-expect (manhattan-distance (make-posn 3 4)) 7)
+(check-expect (manhattan-distance (make-posn 3 2)) 5)
+(check-expect (manhattan-distance (make-posn 2 2)) 4)
+(check-expect (manhattan-distance (make-posn 0 2)) 2)
+
+;(define (manhattan-distance p)
+;  ( ... (posn-x p) ...
+;    ... (posn-y p) ...))
+
+(define (manhattan-distance p)
+  (+ (posn-x p) (posn-y p)))
+
+; It does not matter which strategy is followed, unless going in the opposite direction.
