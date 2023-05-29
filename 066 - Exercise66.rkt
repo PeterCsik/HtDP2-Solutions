70394da2a8bfc58210b689aaf6dcd0aa6c5564ae
PeterCsik <62175713+PeterCsik@users.noreply.github.com>
2023-05-29 08:04:24 +0200

    Create 066 - Exercise66.rkt

---
+++ 066 - Exercise66.rkt
@@ -0,0 +1,14 @@
+(define-struct movie [title producer year])
+(make-movie "Cube" "Mehra Meh, Betty Orr" 1997)
+
+(define-struct person [name hair eyes phone])
+(make-person "Adam" "gray" "green" "001-587-588")
+
+(define-struct pet [name number])
+(make-pet "Hugo" "25879")
+
+(define-struct CD [artist title price])
+(make-CD "Coldpla" "Music of the Spheres" "Grammy 2023") 
+
+(define-struct sweater [material size producer])
+(make-sweater "silk" "S" "Prada")
