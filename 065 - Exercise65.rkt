7eca65e4614b46a7df8f1b284459bb53ddfa20ee
PeterCsik <62175713+PeterCsik@users.noreply.github.com>
2023-06-04 19:30:29 +0200

    Update 065 - Exercise65.rkt

--- 065 - Exercise65.rkt
+++ 065 - Exercise65.rkt
@@ -1,33 +1,24 @@
 (define-struct movie [title producer year])
-
 ; constructor: make-movie
 ; selectors: movie-title, movie-producer, movie-year
 ; predicate: movie?
 
-
 (define-struct person [name hair eyes phone])
-
 ; constructor: make-person
 ; selectors: person-name, person-hair, person-eyes, person-phone
 ; predicate: person?
 
-
 (define-struct pet [name number])
-
 ; constructor: make-pet
 ; selectors: pet-name, pet-number
 ; predicate: pet?
 
-
 (define-struct CD [artist title price])
-
 ; constructor: make-CD
 ; selectors: CD-artist, CD-title, CD-price
 ; predicate: CD?
 
-
 (define-struct sweater [material size producer])
-
 ; constructor: make-sweater
 ; selectors: sweater-material, sweater-size, sweater-producer
 ; predicate: sweater?
