4c52d7e4c2b34d00d7ce581ee877e02814e2c259
PeterCsik <62175713+PeterCsik@users.noreply.github.com>
2023-06-10 10:17:10 +0200

    Create 077 - Exercise77.rkt

---
+++ 077 - Exercise77.rkt
@@ -0,0 +1,5 @@
+(define-struct point-in-time [hour minute second])
+
+; A Point-in-time is a structure:
+;   (make-point-in-time Number Number Number)
+; interpretation represents point in time in hours, minutes and seconds
