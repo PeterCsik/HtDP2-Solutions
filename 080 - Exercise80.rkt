ad0d739c4fb18877dce3c2ef63e3d022a2bce5fc
PeterCsik <62175713+PeterCsik@users.noreply.github.com>
2023-06-12 19:43:09 +0200

    Create 080 - Exercise80.rkt

---
+++ 080 - Exercise80.rkt
@@ -0,0 +1,15 @@
+(define-struct movie [title producer year])
+;(define (best-movie p)
+;  (... (movie-title p) ... (movie-producer p) ... (movie-year p)))
+
+(define-struct pet [name number])
+;(define (my-pet x)
+;  (... (pet-name x) ... (pet-number x) ...))
+
+(define-struct CD [artist title price])
+;(define (choose-CD c)
+;  (... (CD-artist c) ... (CD-title c) ... (CD-price c)))
+
+(define-struct sweater [material size producer])
+;(define (shop-sweater s)
+;  (... (sweater-material s) ... (sweater-size s) ... (sweater-producre s)))
