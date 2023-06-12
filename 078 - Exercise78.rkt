d1458513cf463c1fe71afef26ad456c33f9a6452
PeterCsik <62175713+PeterCsik@users.noreply.github.com>
2023-06-12 20:01:52 +0200

    Update 078 - Exercise78.rkt

--- 078 - Exercise78.rkt
+++ 078 - Exercise78.rkt
@@ -1,5 +1,5 @@
 (define-struct three-letter-word [first-letter second-letter third-letter])
 ; A Three-letter-word is a structure:
-;   (make-three-letter-word 1String 1String 1 String)
+;   (make-three-letter-word 1String 1String 1String)
 ; interpretation represents a three-letter word that consist of
 ; lowercase letters "a" through "z" or #false
