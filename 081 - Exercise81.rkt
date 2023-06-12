a222750f55edd88c0ee4f38ce240fbfc6854b53c
PeterCsik <62175713+PeterCsik@users.noreply.github.com>
2023-06-12 19:56:30 +0200

    Create 081 - Exercise81.rkt

---
+++ 081 - Exercise81.rkt
@@ -0,0 +1,21 @@
+(define-struct point-in-time [hour minute second])
+; A Point-in-time is a structure:
+;   (make-point-in-time Number Number Number)
+; interpretation represents point in time in hours, minutes and seconds
+
+(define ex1 (make-point-in-time 12 30 2))
+
+; point-in-time -> Number
+; produces the number of seconds that have passed since midnight
+; (define (time->seconds p) 0))
+
+(check-expect (time->seconds ex1) 45002)
+
+;(define (time->seconds p)
+;  (... (point-in-time-hour p) ... (point-in-time-minute p)
+;       ... (point-in-time-second p)))
+
+(define (time->seconds p)
+  (+ (* (point-in-time-hour p) 60 60)
+     (* (point-in-time-minute p) 60)
+     (point-in-time-second p)))
