526f75c418b19a608d557027d475b4cc77230e78
PeterCsik <62175713+PeterCsik@users.noreply.github.com>
2023-06-14 20:09:39 +0200

    Create 083 - Exercise83.rkt

---
+++ 083 - Exercise83.rkt
@@ -0,0 +1,45 @@
+(define-struct editor [pre post])
+; An Editor is a structure:
+;   (make-editor String String)
+; interpretation (make-editor s t) describes an editor
+; whose visible text is (string-apend s t) with
+; the cursor displayed between s and t
+
+(define ex1 (make-editor "hello" "world"))
+(define ex2 (make-editor "Silicon" "Valley"))
+(define ex3 (make-editor "Knight" "Rider"))
+
+
+(define CURSOR (rectangle 1 20 "solid" "red"))
+(define SCENE (empty-scene 200 20))
+(define TEXT-SIZE 16)
+(define TEXT-COLOR "black")
+
+
+; Editor -> Image
+; renders the text within an empty space of 200 x 20 pixels
+; (define (render editor) img)
+
+(check-expect (render (make-editor "hello" "world"))
+              (overlay/align "left" "center"
+                      (beside
+                       (text "hello" TEXT-SIZE TEXT-COLOR)
+                       CURSOR
+                       (text "world" TEXT-SIZE TEXT-COLOR))
+                      SCENE))
+
+;(define (render text)
+;  (... (editor-pre text) ... (editor-post text) ...))
+
+;(define txt (make-editor "hello" "world"))
+
+(define (render txt)
+  (overlay/align "left" "center"
+                 (beside
+                  (text (editor-pre txt) TEXT-SIZE TEXT-COLOR)
+                  CURSOR
+                  (text (editor-post txt) TEXT-SIZE TEXT-COLOR))
+                 SCENE))
+
+
+
