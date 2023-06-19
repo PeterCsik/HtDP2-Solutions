6ca15622cc1963ece93a698edd233a7fb2327c19
PeterCsik <62175713+PeterCsik@users.noreply.github.com>
2023-06-19 19:17:27 +0200

    Update 084 - Exercise84.rkt

--- 084 - Exercise84.rkt
+++ 084 - Exercise84.rkt
@@ -1,16 +1,22 @@
+; ---------- Constant definitions ----------
+
+(define CURSOR (rectangle 1 20 "solid" "red"))
+(define SCENE (empty-scene 200 20))
+(define TEXT-SIZE 16)
+(define TEXT-COLOR "black")
+
 ; ---------- Data definitions ----------
 
 (define-struct editor [pre post])
 ; An Editor is a structure:
 ;   (make-editor String String)
 ; interpretation (make-editor s t) describes an editor
-; whose visible text is (string-apend s t) with
+; whose visible text is (string-append s t) with
 ; the cursor displayed between s and t
 
 (define ed1 (make-editor "hello" "world"))
 (define ed2 (make-editor "Silicon" "Valley"))
 (define ed3 (make-editor "Knight" "Rider"))
-
 
 ; A KeyEvent is one of:
 ; -- the single character "a" through "z"
@@ -19,6 +25,33 @@
 ; -- right arrow
 
 ; ---------- Function definitions ----------
+
+;; ---------- "Render" function ----------
+
+; Editor -> Image
+; develops an image that renders the text within an empty scene
+; (define (render ed) img)
+
+(check-expect (render (make-editor "hello" "world"))
+              (overlay/align "left" "center"
+                      (beside
+                       (text "hello" TEXT-SIZE TEXT-COLOR)
+                       CURSOR
+                       (text "world" TEXT-SIZE TEXT-COLOR))
+                      SCENE))
+
+; (define (render ed)
+;   (... (editor-pre ed) ... (editor-post ed) ...))
+
+(define (render ed)
+  (overlay/align "left" "center"
+                 (beside
+                  (text (editor-pre ed) TEXT-SIZE TEXT-COLOR)
+                  CURSOR
+                  (text (editor-post ed) TEXT-SIZE TEXT-COLOR))
+                 SCENE))
+
+;; ---------- "Edit" function ----------
 
 ; Editor KeyEvent -> Editor
 ; -- adds a charachater,
@@ -46,6 +79,8 @@
 
 ;; ---------- Function definitions (Auxiliarry functions) ----------
 
+;;; ---------- Auxiliarry function I ----------
+
 ; Editor KeyEevent -> Editor
 ; adds a single-character to the end of the pre field of ed
 ; (define (add-character ed ke) (make-editor s t))
@@ -57,8 +92,13 @@
 ;        ... ke ... ))
 
 (define (add-character ed ke)
-  (make-editor (string-append (editor-pre ed) ke) (editor-post ed)))
+  (cond
+    [(<(image-width(text (string-append (editor-pre ed)(editor-post ed)) TEXT-SIZE TEXT-COLOR))190)
+  (make-editor (string-append (editor-pre ed) ke) (editor-post ed))]
+   
+    ))
 
+;;; ---------- Auxiliarry function II ----------
 
 ; Editor -> Editor
 ; deletes the character immediatelly to the left  of the cursor
@@ -73,6 +113,7 @@
 (define (delete-character ed)
   (make-editor (substring (editor-pre ed) 0 (-(string-length (editor-pre ed)) 1)) (editor-post ed)))
 
+;;; ---------- Auxiliarry function III ----------
 
 ; Editor KeyEvent -> Editor
 ; moves the cursor one character to the left (if any)
@@ -87,6 +128,7 @@
 (define (move-cursor-left ed)
   (make-editor (substring (editor-pre ed) 0 (- (string-length (editor-pre ed)) 1)) (string-append (string-last (editor-pre ed)) (editor-post ed))))
   
+;;; ---------- Auxiliarry function IV ----------
 
 ; Editor KeyEvent -> Editor
 ; moves the cursor one character to the right (if any)
@@ -98,13 +140,13 @@
 ;   (... (editor-pre ed) ... (editor-post ed)
 ;        ... ke ... ))
 
-;(define (move-cursor-right ed ke)
-
 (define (move-cursor-right ed)
   (make-editor (string-append (editor-pre ed) (string-first (editor-post ed))) (substring (editor-post ed) 1 (string-length (editor-post ed))))) 
 
+;;; ---------- Auxiliarry function V ----------
+
 ; String -> String
-; return the first character of the string
+; returns the first character of the string
 ; (define (string-first x) "a")
 
 (check-expect (string-first "hello") "h")
@@ -116,8 +158,10 @@
   (if (> (string-length x) 0) (string-ith x 0) "Empty string")
   )
 
+;;; ---------- Auxiliarry function VI ----------
+
 ; String -> String
-; return the first character of the strign
+; returns    the first character of the strign
 ; (define (string-last x) "a")
 
 (check-expect (string-last "hello") "o")
