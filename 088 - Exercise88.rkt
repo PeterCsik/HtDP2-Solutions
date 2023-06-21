3a4c754ce68d4aea5c6cca53df7746e98ea14b2f
PeterCsik <62175713+PeterCsik@users.noreply.github.com>
2023-06-21 17:38:59 +0200

    Create 088 - Exercise88.rkt

---
+++ 088 - Exercise88.rkt
@@ -0,0 +1,6 @@
+(define-struct VCat (x-coordinate happiness)
+; VCat is a structure:
+;   (make-VCat Number Number)
+; interpretation (make-VCat x h) keeps the cat's x-coordinate
+; (in pixels from the left margin),and its happiness
+; (a number from 0 to 100)
