d21b874976c72dc4f87fda2f5126e3bc3b32277e
PeterCsik <62175713+PeterCsik@users.noreply.github.com>
2023-06-26 16:58:01 +0200

    Update 088 - Exercise88.rkt

--- 088 - Exercise88.rkt
+++ 088 - Exercise88.rkt
@@ -2,5 +2,5 @@
 ; A VCat is a structure:
 ;   (make-VCat Number Number)
 ; interpretation (make-VCat x h) keeps the cat's x-coordinate
-; (in pixels from the left margin),and its happiness
+; (in pixels from the left margin), and its happiness
 ; (a number from 0 to 100)
