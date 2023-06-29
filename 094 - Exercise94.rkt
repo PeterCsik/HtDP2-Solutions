0258db3298d27f10accf67536dc705d2286c7504
PeterCsik <62175713+PeterCsik@users.noreply.github.com>
2023-06-29 19:39:56 +0200

    Update 094 - Exercise94.rkt

--- 094 - Exercise94.rkt
+++ 094 - Exercise94.rkt
@@ -1,28 +1,33 @@
 ; ---------- Constant definitions ----------
 
+(define SCALE-UNIT 5)
+
 (define UFO (overlay/align "center" "middle"
-                           (ellipse 40 5 "solid" "light gray")
-                           (circle 5 "solid" "dark gray")))
+                           (ellipse (* 10 SCALE-UNIT) (* 2 SCALE-UNIT) "solid" "light gray")
+                           (circle (* 2 SCALE-UNIT) "solid" "dark gray")))
 
-(define TANK (rectangle 25 10 "solid" "light green"))
+(define TANK (rectangle (* 5 SCALE-UNIT) (* 2 SCALE-UNIT) "solid" "light green"))
 
-(define WIDTH 250)    
-(define HEIGHT 150)
+(define MISSILE (triangle (* 2 SCALE-UNIT) "solid" "red"))
+
+(define WIDTH (* 50 SCALE-UNIT))    
+(define HEIGHT (* 50 SCALE-UNIT))
 (define SCENE (empty-scene WIDTH HEIGHT)) ;short for empty scene
 
 (define HILLS (overlay/align/offset "left" "bottom"
-                             (triangle 140 "solid" "dark blue")
-                             -40 -10
-                             (triangle 120 "solid" "blue")
+                             (triangle (* 40 SCALE-UNIT) "solid" "dark blue")
+                             (* -8 SCALE-UNIT) (* -2 SCALE-UNIT)
+                             (triangle (* 30 SCALE-UNIT) "solid" "blue")
                               ))
 
 (define BACKGROUND (overlay/align/offset "right" "bottom"
                                   HILLS
-                                  0 10
+                                  0 (* 2 SCALE-UNIT)
                                   SCENE
                                   ))
 
 (define SCENERY (overlay/align "middle" "bottom"
-                 UFO
-                 TANK
-                 BACKGROUND))
+                               MISSILE
+                               TANK
+                               UFO
+                               BACKGROUND))
