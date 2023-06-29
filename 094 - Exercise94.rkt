40ba77550fb155a486d8fa09226f80d669c1b635
PeterCsik <62175713+PeterCsik@users.noreply.github.com>
2023-06-29 19:13:56 +0200

    Update 094 - Exercise94.rkt

--- 094 - Exercise94.rkt
+++ 094 - Exercise94.rkt
@@ -1,1 +1,24 @@
+; ---------- Constant definitions ----------
 
+(define UFO (overlay/align "center" "middle" (ellipse 40 5 "solid" "light gray") (circle 5 "solid" "dark gray")))
+(define TANK (rectangle 25 10 "solid" "light green"))
+
+(define WIDTH 250)    
+(define HEIGHT 150)
+(define SCENE (empty-scene WIDTH HEIGHT)) ;short for empty scene
+(define HILLS (overlay/align/offset "left" "bottom"
+                             (triangle 140 "solid" "dark blue")
+                             -40 -10
+                             (triangle 120 "solid" "blue")
+                              ))
+
+(define BACKGROUND (overlay/align/offset "right" "bottom"
+                                  HILLS
+                                  0 10
+                                  SCENE
+                                  ))
+
+(define SCENERY (overlay/align "middle" "bottom"
+                 UFO
+                 TANK
+                 BACKGROUND))
