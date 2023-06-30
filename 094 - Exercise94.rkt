10bcf728cad00d32b3ba2ee1d8dd66547c183a62
PeterCsik <62175713+PeterCsik@users.noreply.github.com>
2023-06-30 19:12:08 +0200

    Update 094 - Exercise94.rkt

--- 094 - Exercise94.rkt
+++ 094 - Exercise94.rkt
@@ -3,21 +3,23 @@
 (define SCALE-UNIT 5)
 
 (define UFO (overlay/align "center" "middle"
-                           (ellipse (* 10 SCALE-UNIT) (* 2 SCALE-UNIT) "solid" "light gray")
-                           (circle (* 2 SCALE-UNIT) "solid" "dark gray")))
+                           (ellipse (* 7 SCALE-UNIT) (* 1 SCALE-UNIT) "solid" "light gray")
+                           (circle (* 1 SCALE-UNIT) "solid" "dark gray")))
 
-(define TANK (rectangle (* 5 SCALE-UNIT) (* 2 SCALE-UNIT) "solid" "light green"))
+(define TANK (rectangle (* 4 SCALE-UNIT) (* 1.5 SCALE-UNIT) "solid" "light green"))
 
-(define MISSILE (triangle (* 2 SCALE-UNIT) "solid" "red"))
+(define MISSILE (triangle (* 1 SCALE-UNIT) "solid" "red"))
 
-(define WIDTH (* 50 SCALE-UNIT))    
-(define HEIGHT (* 50 SCALE-UNIT))
+(define WIDTH (* 40 SCALE-UNIT))    
+(define HEIGHT (* 40 SCALE-UNIT))
 (define SCENE (empty-scene WIDTH HEIGHT)) ;short for empty scene
 
+
+
 (define HILLS (overlay/align/offset "left" "bottom"
-                             (triangle (* 40 SCALE-UNIT) "solid" "dark blue")
+                             (triangle (* 30 SCALE-UNIT) "solid" "dark blue")
                              (* -8 SCALE-UNIT) (* -2 SCALE-UNIT)
-                             (triangle (* 30 SCALE-UNIT) "solid" "blue")
+                             (triangle (* 20 SCALE-UNIT) "solid" "blue")
                               ))
 
 (define BACKGROUND (overlay/align/offset "right" "bottom"
