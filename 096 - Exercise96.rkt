35c2b3cc3b97ff32f222bd582ca69f9dc783cbf4
PeterCsik <62175713+PeterCsik@users.noreply.github.com>
2023-06-30 19:53:21 +0200

    Create 096 - Exercise96.rkt

---
+++ 096 - Exercise96.rkt
@@ -0,0 +1,104 @@
+; ---------- Constant definitions ----------
+
+(define SCALE-UNIT 5)
+
+(define UFO (overlay/align "center" "middle"
+                           (ellipse (* 7 SCALE-UNIT) (* 1 SCALE-UNIT) "solid" "light gray")
+                           (circle (* 1 SCALE-UNIT) "solid" "dark gray")))
+
+(define TANK (rectangle (* 4 SCALE-UNIT) (* 1.5 SCALE-UNIT) "solid" "light green"))
+
+(define MISSILE (triangle (* 1 SCALE-UNIT) "solid" "red"))
+
+(define WIDTH (* 40 SCALE-UNIT))    
+(define HEIGHT (* 40 SCALE-UNIT))
+(define SCENE (empty-scene WIDTH HEIGHT)) ;short for empty scene
+
+
+
+(define HILLS (overlay/align/offset "left" "bottom"
+                             (triangle (* 30 SCALE-UNIT) "solid" "dark blue")
+                             (* -8 SCALE-UNIT) (* -2 SCALE-UNIT)
+                             (triangle (* 20 SCALE-UNIT) "solid" "blue")
+                              ))
+
+(define BACKGROUND (overlay/align/offset "right" "bottom"
+                                  HILLS
+                                  0 (* 2 SCALE-UNIT)
+                                  SCENE
+                                  ))
+
+(define SCENERY (overlay/align "middle" "bottom"
+                               MISSILE
+                               TANK
+                               UFO
+                               BACKGROUND))
+
+
+; ---------- Data definitions ----------
+
+; A UFO is a Posn
+; interpretation (make-posn x y) is the UFO's location
+; (using the top-down, left-to-right convention)
+
+(define-struct tank [loc vel])
+; A Tank is a structure:
+;  (make-tank Number Number)
+; interpretation (make-tank x dx) specifies the position:
+; (x, HEIGHT) and the tank's speed: dx pixels/tick
+
+; A Missile is a Posn
+; interpretation (make-posn x y) is the missile's place
+
+(define-struct aim [ufo tank])
+(define-struct fired [ufo tank missile])
+; A SIGS is one of:
+; -- (make-aim UFO Tank)
+; -- (make-fired UFO Tank Missile)
+; interpretation represents the complete state of a
+; space invader game
+
+; Examples (3 instances):
+
+;(make-aim (make-posn 20 10) (make-tank 28 -3))
+;
+;(make-fired (make-posn 20 10)
+;            (make-tank 28 -3)
+;            (make-posn 28 (- HEIGHT TANK-HEIGHT)))
+;
+;(make-fired (make-posn 20 100)
+;            (make-tank 100 3)
+;            (make-posn 22 103))
+
+; The three instances are generated according to the first or
+; second clause of the data definition because they represent
+; different states of word. The first instance represents the
+; thank's position during aiming at the UFO. The second one
+; shows that a missile was fired. And the last one represents
+; how the position of all objects has changed after the fire of
+; the missile.
+
+; Sketsches of each of three game states
+
+(place-image TANK
+               28 195
+               (place-image
+                UFO
+                20 150
+                BACKGROUND))
+
+(place-image MISSILE
+               28 190
+               (place-image TANK 28 195
+                            (place-image
+                             UFO
+                             20 150
+                             BACKGROUND)))
+
+(place-image MISSILE
+               28 90
+               (place-image TANK 100 195
+                            (place-image
+                             UFO
+                             20 100
+                             BACKGROUND)))
