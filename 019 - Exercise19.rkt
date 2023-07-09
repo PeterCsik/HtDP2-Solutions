105d7a4defe507db196c0991e647012a2303f22c
PeterCsik <62175713+PeterCsik@users.noreply.github.com>
2023-07-10 01:58:38 +0200

    Update 019 - Exercise19.rkt

--- 019 - Exercise19.rkt
+++ 019 - Exercise19.rkt
@@ -1,6 +1,2 @@
 (define (string-insert str i)
-  (cond
-    [(=(string-length str) 0) "Error: It is an empty string"]
-    [(or (<= i 0)(> i (string-length str)))(string-append "Number "(number->string i)" is out of the valid range [1, " (number->string(string-length str)) "].")]
-    [(and (> (string-length str) 0)(<= i (string-length str)))(string-append(substring str 0 (- i 1))"_"(substring str (- i 1)))]
-    ))
+  (string-append (substring str 0 (- i 1))"_"(substring str (- i 1))))
