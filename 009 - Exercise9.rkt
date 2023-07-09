c1467758501f980da99c9fc0af319851b39e7af6
PeterCsik <62175713+PeterCsik@users.noreply.github.com>
2023-07-10 01:27:26 +0200

    Update 009 - Exercise9.rkt

--- 009 - Exercise9.rkt
+++ 009 - Exercise9.rkt
@@ -1,19 +1,21 @@
 (define in #false)
 
-(if (string? in) (string-length in)
-(if (image? in) (* (image-width in) (image-height in))
-(if (boolean? in) (if in 10 20)
-(if (and (number? in) (> in 0)) (- in 1) in))))
+(if (string? in)
+    (string-length in)
+    (if (image? in)
+        (* (image-width in) (image-height in))
+        (if (boolean? in)
+            (if in
+                10
+                20)
+            (if (and (number? in)(> in 0))
+                in))))
 
-(cond                                                       ; an  alternative solution using cond expressions
-  [(string? in) (string-length in)]
-  [(image? in) (* (image-width in)(image-height in))]
-  [(boolean? in)
-   (cond
-     [(and in #true) 10]
-     [else 20])]
-  [(number? in)
-   (cond
-     [(> in 0) (- in 1)]
-     [(<= in 0) in])]
-  )
+(cond [(string? in) (string-length in)]
+      [(image? in)  (* (image-width in)(image-height in))]
+      [(boolean? in)
+       (cond [(and in #true) 10]
+             [else           20])]
+      [(number? in)
+       (cond [(> in 0) (- in 1)]
+             [(<= in 0) in])])
