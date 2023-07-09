39db68f406a031562c3fd3a633cd6cbc2d79725c
PeterCsik <62175713+PeterCsik@users.noreply.github.com>
2023-07-10 01:11:16 +0200

    Update 005 - Exercise5.rkt

--- 005 - Exercise5.rkt
+++ 005 - Exercise5.rkt
@@ -1,7 +1,6 @@
 (define BOAT-SIZE 50)
 
 (above (triangle (* BOAT-SIZE 3) "solid" "grey")
-         (beside
-               (triangle/sas BOAT-SIZE 90 BOAT-SIZE "solid" "brown")
-               (rectangle (* BOAT-SIZE 5) BOAT-SIZE "solid" "brown")
-               (triangle/asa 90 BOAT-SIZE 45 "solid" "brown")))
+         (beside (triangle/sas BOAT-SIZE 90 BOAT-SIZE "solid" "brown")
+                 (rectangle (* BOAT-SIZE 5) BOAT-SIZE "solid" "brown")
+                 (triangle/asa 90 BOAT-SIZE 45 "solid" "brown")))
