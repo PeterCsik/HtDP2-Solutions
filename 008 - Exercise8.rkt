2cf79a05cad922cd71677c271e7207c855b5298b
PeterCsik <62175713+PeterCsik@users.noreply.github.com>
2023-07-10 02:30:48 +0200

    Update 008 - Exercise8.rkt

--- 008 - Exercise8.rkt
+++ 008 - Exercise8.rkt
@@ -1,11 +1,11 @@
-(define picture ... )
-
+(define cat ... )
 (if (=(image-height picture)(image-width picture))
     "square"
     (if (>(image-height picture)(image-width picture))
         "tall"
         "wide"))
 
+(define cat1 ...)                                                    ;; an alternative solution
 (cond [(=(image-height picture)(image-width picture)) "square"]
       [(>(image-height picture)(image-width picture)) "tall"]
       [else                                           "wide"])
