9d8d832c842656e280e997937426403193b7ff8d
PeterCsik <62175713+PeterCsik@users.noreply.github.com>
2023-07-10 12:18:02 +0200

    Update 025 - Exercise25.rkt

--- 025 - Exercise25.rkt
+++ 025 - Exercise25.rkt
@@ -1,34 +1,30 @@
 (define (image-classify img)
-  (cond
-   ((>= (image-height img) (image-width img)) "tall")
-   ((= (image-height img) (image-width img)) "square")
-   ((<= (image-height img) (image-width img)) "wide")))
+  (cond ((>= (image-height img) (image-width img)) "tall")
+        ((= (image-height img) (image-width img))  "square")
+        ((<= (image-height img) (image-width img)) "wide")))
    
-(image-classify ...)                                   ; image of a rocket was used and it returned "tall"
+(image-classify ...)                                           ; image of a rocket was used
+                                                               
 
-; (cond
-; ((>= (image-height ...) (image-width ...)) "tall")
-; ((= (image-height ...) (image-width ...)) "square")
-; ((<= (image-height ...) (image-width ...)) "wide"))
-
-; (cond
-; ((>= 42 (image-width .)) "tall")
-; ((= (image-height .) (image-width .)) "square")
-; ((<= (image-height .) (image-width .)) "wide"))
-
-; (cond
-; ((>= 42 28) "tall")
-; ((= (image-height .) (image-width .)) "square")
-; ((<= (image-height .) (image-width .)) "wide"))
-
-; (#true "tall")
-; ((= (image-height .) (image-width .)) "square")
-; ((<= (image-height .) (image-width .)) "wide"))
-; "tall"
+;; (cond ((>= (image-height ...) (image-width ...)) "tall")
+;;       ((= (image-height ...) (image-width ...))  "square")
+;;       ((<= (image-height ...) (image-width ...)) "wide"))
+;;
+;; (cond ((>= 42 (image-width ...))                 "tall")
+;;       ((= (image-height ...) (image-width ...))  "square")
+;;       ((<= (image-height ...) (image-width ...)) "wide"))
+;;
+;; (cond ((>= 42 28)                                "tall")
+;;       ((= (image-height ...) (image-width ...))  "square")
+;;       ((<= (image-height ...) (image-width ...)) "wide"))
+;;
+;; (#true                                     "tall")
+;; ((= (image-height ...) (image-width ...))  "square")
+;; ((<= (image-height ...) (image-width ...)) "wide"))
+;; "tall"                                                      ; the function returned "tall"
 
 
-(define (image-classify img)                                ; fixed version of the function
-  (cond
-   ((> (image-height img) (image-width img)) "tall")        ; >= was changed to >
-   ((= (image-height img) (image-width img)) "square")
-   ((<= (image-height img) (image-width img)) "wide")))     ; <= was changed to <
+(define (image-classify img)                                   ; the fixed version of the function
+  (cond ((> (image-height img) (image-width img))  "tall")     ; >= was changed to >
+        ((= (image-height img) (image-width img))  "square")
+        ((<= (image-height img) (image-width img)) "wide")))   ; <= was changed to <
