873f951d3834bf680219097e9c550200bff4a52e
PeterCsik <62175713+PeterCsik@users.noreply.github.com>
2023-07-10 13:11:37 +0200

    Update 036 - Exercise36.rkt

--- 036 - Exercise36.rkt
+++ 036 - Exercise36.rkt
@@ -1,15 +1,15 @@
-; An image area is a (positive non-zero) Number.
-; Interpretation: it represents the count of pixels in a given image.
+;; An image area is a (positive non-zero) Number.
+;; interpretation: it represents the count of pixels in a given image
 
-; Image -> Number
-; Counts the number of pixels in a given image.
-; (define (image-area img) 0)
+;; Image -> Number
+;; counts the number of pixels in a given image
+;; (define (image-area img) 0)
 
-; given: an image with heigth of 100 pixels and width of 20 pixels, expected: 2000
-; given: an image with heigth of 100 pixels and width of 100 pixels, expected: 10000
+;; given: an image with heigth of 100 pixels and width of 20 pixels, expected: 2000
+;; given: an image with heigth of 100 pixels and width of 100 pixels, expected: 10000
 
-; (define (image-area img)
-;   (... img ...))
+;; (define (image-area img)
+;;   (... img ...))
 
 (define (image-area img)
   (* (image-height img)(image-width img)))
