1b182e61e36210743f2382b6820cb34a9e175fa4
PeterCsik <62175713+PeterCsik@users.noreply.github.com>
2023-07-10 12:28:43 +0200

    Update 031 - Exercise31.rkt

--- 031 - Exercise31.rkt
+++ 031 - Exercise31.rkt
@@ -1,23 +1,21 @@
 (define (letter fst lst signature-name)
-  (string-append
-    (opening fst)
-    (body fst lst)
-    (closing signature-name)))
+  (string-append (opening fst)
+                 (body fst lst)
+                 (closing signature-name)))
  
 (define (opening fst)
-  (string-append "Dear " fst ", "))
+  (string-append "Dear "
+                 fst
+                 ", "))
  
 (define (body fst lst)
-  (string-append
-   "We have discovered that all people with the "
-   "last name " lst " have won our lottery. So, "
-   fst ", " "hurry and pick up your prize. "))
+  (string-append "We have discovered that all people with the "
+                 "last name " lst " have won our lottery. So, "
+                 fst ", " "hurry and pick up your prize. "))
  
 (define (closing signature-name)
-  (string-append
-   "Sincerely, "
-   signature-name
-    ))
+  (string-append "Sincerely, "
+                 signature-name))
 
 (define (main in-fst in-lst in-signature out)
   (write-file out
