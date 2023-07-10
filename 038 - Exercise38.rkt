445e84e3c69588cd22402f29be90b4d3d68131fa
PeterCsik <62175713+PeterCsik@users.noreply.github.com>
2023-07-10 13:13:45 +0200

    Update 038 - Exercise38.rkt

--- 038 - Exercise38.rkt
+++ 038 - Exercise38.rkt
@@ -1,19 +1,19 @@
-; A non-empty string is represented by String (data type).
-; Interpretation: a non-empty string is a sequence of characters with at least one character.
+;; A non-empty string is represented by String (data type).
+;; interpretation: a non-empty string is a sequence of characters with at least one character
 
-; (Non-empty) String -> String
-; Produces a string like the given one with the last character removed.
-; (define (string-remove-last str) "a")
+;; (Non-empty) String -> String
+;; produces a string like the given one with the last character removed
+;; (define (string-remove-last str) "a")
 
-; given: "abc", expected: "ab"
-; given: "hello world", expected: "hello worl"
-; given: "b", expected: ""
+;; given: "abc", expected: "ab"
+;; given: "hello world", expected: "hello worl"
+;; given: "b", expected: ""
 
-; (define (string-rest str)
-;   (... str ...))
+;; (define (string-rest str)
+;;   (... str ...))
 
 (define (string-remove-last str)
-  (substring str 0 (-(string-length str)1)))
+  (substring str 0 (-(string-length str) 1)))
 
 (check-expect (string-remove-last "abc") "ab")
 (check-expect (string-remove-last "hello world") "hello worl")
