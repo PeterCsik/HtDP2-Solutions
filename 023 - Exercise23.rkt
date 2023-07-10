689660f8380cdff00ad1fe11634d313bf61fe2dd
PeterCsik <62175713+PeterCsik@users.noreply.github.com>
2023-07-10 12:04:04 +0200

    Update 023 - Exercise23.rkt

--- 023 - Exercise23.rkt
+++ 023 - Exercise23.rkt
@@ -1,5 +1,5 @@
 (define (string-first s)
   (substring s 0 1))
 
-(string-first "abc")          ;; (substring "abc" 0 1)
-                              ;; "a"
+(string-first "abc")          ; (substring "abc" 0 1)
+                              ; "a"
