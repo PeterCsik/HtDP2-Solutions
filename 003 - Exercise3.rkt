b4dd38d74b42e34e12e50e5ae76a12e8fd42bd73
PeterCsik <62175713+PeterCsik@users.noreply.github.com>
2023-07-10 02:16:22 +0200

    Update 003 - Exercise3.rkt

--- 003 - Exercise3.rkt
+++ 003 - Exercise3.rkt
@@ -1,4 +1,8 @@
 (define str "helloworld")
 (define i 5)
 
-(string-append (substring str 0 i) "_" (substring str i (string-length str)))
+(string-append (substring str 0 i)
+               "_"
+               (substring str
+                          i
+                          (string-length str)))
