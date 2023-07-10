a3fd90f6dd1541562118e6a16add3a6b3252ad3b
PeterCsik <62175713+PeterCsik@users.noreply.github.com>
2023-07-10 12:20:13 +0200

    Update 026 - Exercise26.rkt

--- 026 - Exercise26.rkt
+++ 026 - Exercise26.rkt
@@ -3,7 +3,7 @@
                  "_"
                  (substring s i)))
  
-(string-insert "helloworld" 6)                                         ; expected value "hellow_orld"
+(string-insert "helloworld" 6)                                    ; expected value "hellow_orld"
 
 ;; (string-append
 ;; (substring "helloworld" 0 6)
@@ -15,4 +15,4 @@
 ;; (string-append (substring s 0 i) "_" (substring s i)))
 ;; (string-append "hellow" "_" "orld")
 ;;
-;; "hellow_orld"                                                       ; returns the expected value
+;; "hellow_orld"                                                  ; returns the expected value
