921e8a5b067f1c65c35b9074293ae8fd5d55e64f
PeterCsik <62175713+PeterCsik@users.noreply.github.com>
2023-07-10 12:06:19 +0200

    Update 008 - Exercise8.rkt

--- 008 - Exercise8.rkt
+++ 008 - Exercise8.rkt
@@ -5,7 +5,7 @@
         "tall"
         "wide"))
 
-(define cat1 ...)                                                    ;; an alternative solution
+(define cat1 ...)                                                    ; an alternative solution
 (cond [(=(image-height picture)(image-width picture)) "square"]
       [(>(image-height picture)(image-width picture)) "tall"]
       [else                                           "wide"])
