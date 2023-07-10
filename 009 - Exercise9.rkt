d9c7522e26156da21676b39c2a582239f0ec46a2
PeterCsik <62175713+PeterCsik@users.noreply.github.com>
2023-07-10 03:18:42 +0200

    Update 009 - Exercise9.rkt

--- 009 - Exercise9.rkt
+++ 009 - Exercise9.rkt
@@ -3,7 +3,7 @@
       [(image? x)  (* (image-width x)(image-height x))]
       [(boolean? x)
        (cond [(and x #true) 10]
-             [else           20])]
+             [else          20])]
       [(number? x)
        (cond [(> x 0) (- x 1)]
              [(<= x 0) x])]))
