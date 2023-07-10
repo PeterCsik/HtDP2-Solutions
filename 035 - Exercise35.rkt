b6fde6e933fc4401a1fc9bda7b06f2173a242e84
PeterCsik <62175713+PeterCsik@users.noreply.github.com>
2023-07-10 13:10:09 +0200

    Update 035 - Exercise35.rkt

--- 035 - Exercise35.rkt
+++ 035 - Exercise35.rkt
@@ -1,16 +1,16 @@
-; A non-empty string is represented by String (data type).
-; Interpretation: a non-empty string is a sequence of characters with at least one character.
+;; A non-empty string is represented by String (data type).
+;; interpretation: a non-empty string is a sequence of characters with at least one character
 
-; (Non-empty) String -> String
-; Extracts the last character from a non-empty string.
-; (define (string-last str) "a")
+;; (Non-empty) String -> String
+;; Extracts the last character from a non-empty string.
+;; (define (string-last str) "a")
 
-; given: "abc", expected: "c"
-; given: "hello world", expected: "d"
-; given: "b", expected: "b"
+;; given: "abc", expected: "c"
+;; given: "hello world", expected: "d"
+;; given: "b", expected: "b"
 
-; (define (string-last str)
-;   (... str ...))
+;; (define (string-last str)
+;;   (... str ...))
 
 (define (string-last str)
   (string-ith str (-(string-length str)1)))
