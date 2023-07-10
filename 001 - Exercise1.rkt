6cbb6c7048b8752505f32ed219ed81d9f78d4829
PeterCsik <62175713+PeterCsik@users.noreply.github.com>
2023-07-10 03:21:50 +0200

    Update 001 - Exercise1.rkt

--- 001 - Exercise1.rkt
+++ 001 - Exercise1.rkt
@@ -1,8 +1,6 @@
 (define x 3)
 (define y 4)
 
-(sqrt (+ (sqr x)
-         (sqr y)))
+(sqrt (+ (sqr x) (sqr y)))
 
-(sqrt (+ (* x x)      ;; an alternative solution
-         (* y y)))   
+(sqrt (+ (* x x) (* y y)))        ;; an alternative solution
